82fd5af1b8487ef33f59c1ae7fb9b42b83c7169e
javiercrigar <javicriado28@gmail.com>
2023-03-19 22:59:44 +0100

    Arreglado reseteo de fragments

--- app/src/main/java/com/example/proyecto1/Home.java
+++ app/src/main/java/com/example/proyecto1/Home.java
@@ -1,9 +1,8 @@
 package com.example.proyecto1;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,35 +14,54 @@ import com.example.proyecto1.databinding.ActivityHomeBinding;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class Home extends AppCompatActivity {
+    //private static final String LAST_FRAGMENT_KEY = "last_fragment";
+    private String lastFragment;
 
     ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        if (savedInstanceState != null) {
+            lastFragment = savedInstanceState.getString("fragment_act");
+            if (lastFragment != null) {
+                restoreLastFragment();
+            } else {
+                replaceFragment(new MainFragment());
+            }
+        } else {
+            replaceFragment(new MainFragment());
+        }
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragment(new MainFragment());
+
 
         binding.navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.navigation_settings:;
+                        lastFragment = "settings";
                         replaceFragment(new SettingsFragment());
                         break;
                     case R.id.navigation_main:;
+                        lastFragment = "main";
                         replaceFragment(new MainFragment());
                         break;
                     case R.id.navigation_recetas:;
+                        lastFragment = "recetas";
                         replaceFragment(new RecetasFragment());
-
                         break;
                 }
                 return true;
             }
         });
     }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("fragment_act", lastFragment);
+    }
+
     //Metodo para reemplazar los fragment
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager= getSupportFragmentManager();
@@ -51,4 +69,17 @@ public class Home extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout,fragment);
         fragmentTransaction.commit();
     }
-}
+    private void restoreLastFragment() {
+        switch (lastFragment) {
+            case "settings":
+                replaceFragment(new SettingsFragment());
+                break;
+            case "main":
+                replaceFragment(new MainFragment());
+                break;
+            case "recetas":
+                replaceFragment(new RecetasFragment());
+                break;
+        }
+
+}}
